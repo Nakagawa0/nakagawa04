20a59053cbe439b50654b644c0fd9abe50d1c027
nakagawa0 <a14shoma@gmail.com>
2025-05-22 16:21:23 +0000

    dashboard3

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,8 +1,9 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app') -->
 
-@section('content')
+<!-- @section('content') -->
 <div style="text-align: right; margin-bottom: 20px;">
     <a href="{{ route('login') }}" class="btn-primary">ログイン</a>
+    <a href="{{ route('register') }}" class="btn-primary" style="margin-left: 10px;">新規登録</a>
 </div>
 <div class="hero bg-cover bg-center min-h-screen flex flex-col items-center justify-center text-white" style="background-image: url('https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=1500&q=80');">
     <div class="bg-black bg-opacity-60 p-10 rounded shadow-lg text-center">
@@ -11,6 +12,7 @@
         <div class="flex flex-col sm:flex-row gap-4 justify-center">
             <a href="{{ route('recipes.create') }}" class="btn-main">レシピを記録する</a>
             <a href="{{ route('user_ingredients.create') }}" class="btn-sub">食材を登録する</a>
+            <a href="/chart" class="btn-sub">栄養バランスをみる</a> {{-- 追加ボタン --}}
         </div>
     </div>
 </div>
